588db156ed36fd83eb4b24d49806647c6ce091c2
NadilaZoroLuffy <aprillanadila740@gmail.com>
2022-11-18 10:34:50 +0700

    first commit

---
+++ cba/index.php
@@ -0,0 +1,203 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Projek percobaan cv</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+</head>
+<body>
+    <!-- Start Navbar-->
+<section id="project">
+<nav class="navbar navbar-expand-lg bg-light fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="#">Nadila Aprilla</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#project">Project</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#journey">Journey</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#project">Service</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#projects">Content</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </section>
+
+    <!-- End Navbar-->
+  <!--Start Jumboton-->
+
+ <section class="jumbotron">
+    <div class="container">
+        <div class="row">
+    <div class="col-6">
+        <h1> I’m Frontend Website Developers</h1>
+        <p class="labels pt-3">Freelance Frontend Website Developers and Product Designers 
+          based on Jakarta, Indonesia  </p>
+        <button type="submit" name="submit" class="tombol">Hire Me</button>
+        </div>
+        <div class="col-6">
+            <img src="../imges/img-hero.png"  class="img-fluid rounded-circle">
+        </div>
+    </div>
+    </div>
+          </section>
+
+  <!-- End Jumboton-->
+
+      <!-- Start journey-->
+
+      <section id="journey" class="about-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-12 col-lg-3">
+                    <h3>My Recent Client</h3><br>
+                    <p class="labels pt-2">Perusahaan - perusahaan besar yang sudah
+                      bekerja sama dengan saya</p>
+                </div>
+                <div class="col-6 col-md-6 col-lg-2 col-xl-2 mb-3">
+                    <img src="../imges/logo-tokopedia.png">
+                    <img src="../imges/logo-google.png" class="py-5">
+                </div>
+                <div class="col-6 col-md-6 col-lg-2 col-xl-2 mb-3">
+                    <img src="../imges/logo-gojek.png">
+                    <img src="../imges/Instagram Logo.png" class="py-5">
+                </div>
+                <div class="col-6 col-md-6 col-lg-2 col-xl-2 mb-3">
+                    <img src="../imges/logo-spotify.png">
+                    <img src="../imges/logo-traveloka.png" class="py-5">
+                </div>
+                <div class="col-6 col-md-6 col-lg-2 col-xl-2 mb-3">
+                    <img src="../imges/logo-grammarly.png">
+                    <img src="../imges/logo-facebook.png" class="py-5">
+                </div>
+            </div>
+            </div>
+        </div><br><br>
+  </section>
+
+      <!-- End journey-->
+
+      <!-- Start journey-->
+      <section id="journey" class="about-section">
+        <div class="container">
+            <div class="row text-center">
+                <div class="col">
+                    <h1>Specialities</h1><br>  
+                </div>
+                <p class="labels pt-3 py-5">Beberapa keahlian saya dalam menangani product digital</p>
+            </div>
+            <div class="row d-flex justify-content-between">
+                <div class="box col-3 mx-2 pb-4 mb-4 col-xl-3 col-md-12 col-12 text-primary border-top border-primary border-3 py-4 px-4">
+                  <img src="../imges/point.jpg" class="fto" alt="">
+                  <h5 class="pt-4 text-dark">Product Thinking</h5>
+                  <p class="label">product thinking is a method of
+                    analyzing a product using a design
+                    thinking framework</p>
+                </div>
+                <div class="box col-3 mx-2 pb-4 mb-4 col-xl-3 col-md-12 col-12 text-primary border-top border-primary border-3 py-4 px-4">
+                  <img src="../imges/ic_uiux.png" class="fto" alt="">
+                  <h5 class="pt-4 text-dark">UI/UX Designers</h5>
+                  <p class="label">Design all screen from client brief
+                    and sometimes build the company
+                    design system</p>
+                </div>
+                <div class="box col-3 mx-2 pb-4 mb-4 col-xl-3 col-md-12 col-12 text-primary border-top border-primary border-3 py-4 px-4">
+                  <img src="../imges/ic_frontend.png" class="fto" alt="">
+                  <h5 class="pt-4 text-dark">Frontend Developer</h5>
+                  <p class="label">The skill is a slicing all component
+                    from design to code and make the
+                    website responsive</p>
+                </div>
+            </div>
+        </div><br><br>
+  </section>
+    <!-- End journey-->
+  
+      <!-- Start Project Section-->
+
+<section id="project" class="Project-Section">
+  <div class="container">
+    <div class="row text-center" style="margin-bottom: 56px">
+      <div class="col">
+          <h1>My prtofolio Project</h1><br>
+          <p class="labels pt-3 py-5">Beberapa Keahlian saya dalam mengenai product digital ini</p>
+      </div>
+  </div>
+  <div class="row justify-content-between mb-md-5 mb-3">
+    <div class="col-md-6 mb-3 mb-md-0">
+        <img src="../imges/img-medium1.png" class="w-100">
+        <h3>Gaming Store Landing Page</h3>
+        <p class="labels pt-3 py-5">Web Designers</p>
+    </div>
+    <div class="col-md-6 mb-md-0">
+        <img src="../imges/img-medium2.png" class="w-100">
+        <h3>UI Design Digital Agency</h3>
+        <p class="labels pt-3 py-5">UI/UX Design</p>
+    </div>
+    <div class="col-md-4 mb-3 mb-md-0">
+        <img src="../imges/img-mini1.png" class="w-100">
+        <h3>E - Learning Landing Page</h3>
+        <p class="labels pt-3 py-5">Web Designers</p>
+    </div>
+    <div class="col-md-4 mb-md-0">
+        <img src="../imges/img-mini2.png" class="w-100">
+        <h3>Travel App Mobile</h3>
+        <p class="labels pt-3 py-5">UI/UX Design</p>
+    </div>
+    <div class="col-md-4 mb-md-0">
+        <img src="../imges/img-mini3.png" class="w-100">
+        <h3>Booking Villa’s App</h3>
+        <p class="labels pt-3 py-5">Web Designers</p>
+    </div>
+    </div>
+  </div>
+  </div>
+</section>
+
+      <!-- End  Project Section-->
+
+      <!-- Start Footer-->
+<section id="projects" class="Project-Section"></section>
+        <footer class="bg-light">
+            <div class="container">
+                <div class="row">
+                    <div class="col-8 col-md-5 col-lg-5 pt-5 py-3">
+                        <h3>Nadila Aprilla</h3>
+                    </div>
+                    <div class="col-8 pt-4 col-md-5 col-lg-5">
+                    <ul id="menu-utama">
+                        <li><a href="">Github</a></li>
+                        <li><a href="">Instagram</a></li>
+                        <li><a href="">Dribble</a></li>
+                        <li><a href="">Behance</a></li>
+                    </ul>
+                </div>
+                    <div class="col-2 pt-4">
+                        <button type="submit" name="submit" class="tombol text-light">Hire Me</button>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+      <!--End  Footer-->
+
+      <!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/d9b1488516.js" crossorigin="anonymous"></script>
+</body>
+</html>
